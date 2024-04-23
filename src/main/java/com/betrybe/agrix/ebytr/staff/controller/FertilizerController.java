c47ec0de6824479f74fed87e561620187b4bcfc1
Willian Gonçalves <willian.p.g@outlook.com>
2024-04-22 21:41:29 -0300

    Requisito 06 - Limitar acesso à rota GET /fertilizers

--- src/main/java/com/betrybe/agrix/ebytr/staff/controller/FertilizerController.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/controller/FertilizerController.java
@@ -8,6 +8,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,7 +50,8 @@ public class FertilizerController {
    * @return return
    */
   @GetMapping()
-  public ResponseEntity<List<FertilizerDto>> getAll() {
+  @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+  public ResponseEntity<List<FertilizerDto>> getAll(@AuthenticationPrincipal Fertilizer fertilizer) {
     List<Fertilizer> fertilizers = fertilizerService.getAll();
     List<FertilizerDto> listFertilizer = fertilizers.stream()
         .map(FertilizerDto::fromEntity).toList();
